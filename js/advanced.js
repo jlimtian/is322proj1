da5f74bbc440d552de4911fc5f30821b9a98f8b0
kimim <jag94@njit.edu>
2021-02-21 15:40:33 -0500

    Ordering works, others...not so much

--- js/advanced.js
+++ js/advanced.js
@@ -1,5 +1,3 @@
-// add alt attributes to the images
-
 var carded = 'carded';
 
 (function () {
@@ -8,23 +6,71 @@ var carded = 'carded';
     console.log(window.carded);
 
     var mockDatabase = [
-        {image:'../images/ducky_lamp.jpg', alt:'Cute Duck Lamp.', name:'Ducky Lamp', _price:'$456.95', _popularity:'002', animal: true, item: false},
-        {image:'../images/balloon_lamp.jpg', alt:'Red Balloon Lamp.', name:'Balloon Lamp', _price:'$28.95', _popularity:'003', animal: false, item: true},
-        {image:'../images/deluxe_jellyfish_lamp.jpg', alt:'Deluxe Jellyfish Lamp.', name:'Jellyfish Lamp', _price:'$1,200.95', _popularity:'001', animal: true, item: false},
-        {image:'../images/pencil_lamp.jpg', alt:'Large Pencil with Squiggle Lamp.', name:'Pencil Lamp', _price:'$52.95', _popularity:'003', animal: false, item: true},
-        {image:'../images/arrow_lamp.jpg', alt:'Lamp made of Arrows.', name:'Arrow Lamp', _price:'$16.95', _popularity:'005', animal: false, item: true},
-        {image:'../images/birds_lamp.jpg', alt:'Perched Birds Lamp.', name:'Bird Lamp', _price:'$65.95', _popularity:'002', animal: true, item: false}
+        {
+            image: '../images/ducky_lamp.jpg',
+            alt: 'Cute Duck Lamp.',
+            name: 'Ducky Lamp',
+            price: '456.95',
+            popularity: '002',
+            animal: true,
+            item: false
+        },
+        {
+            image: '../images/balloon_lamp.jpg',
+            alt: 'Red Balloon Lamp.',
+            name: 'Balloon Lamp',
+            price: '28.95',
+            popularity: '003',
+            animal: false,
+            item: true
+        },
+        {
+            image: '../images/deluxe_jellyfish_lamp.jpg',
+            alt: 'Deluxe Jellyfish Lamp.',
+            name: 'Deluxe Jellyfish Lamp',
+            price: '795.95',
+            popularity: '001',
+            animal: true,
+            item: false
+        },
+        {
+            image: '../images/pencil_lamp.jpg',
+            alt: 'Large Pencil with Squiggle Lamp.',
+            name: 'Pencil Lamp',
+            price: '52.95',
+            popularity: '003',
+            animal: false,
+            item: true
+        },
+        {
+            image: '../images/arrow_lamp.jpg',
+            alt: 'Lamp made of Arrows.',
+            name: 'Arrow Lamp',
+            price: '16.95',
+            popularity: '005',
+            animal: false,
+            item: true
+        },
+        {
+            image: '../images/birds_lamp.jpg',
+            alt: 'Perched Birds Lamp.',
+            name: 'Bird Lamp',
+            price: '65.95',
+            popularity: '002',
+            animal: true,
+            item: false
+        }
     ];
 
-    function renderList (results) {
+    function renderList(results) {
         // Grab the div to place the cards, clear it out in case it was already populated
         var cardBody = document.querySelector('#container');
         cardBody.innerHTML = '';
 
         // Maps all attributes to a card
         var tableRows = results.map(function (result) {
-            return '<div class="card"><a href="#"><div><img src="' + result.image + '" alt="' + result.alt + '"></div><div class="card-label"><div class="card-name">' + result.name + '</div><div class="card-price">' +
-                result._price + '</div></div></a></div>';
+            return '<div class="card"><a href="#"><div><img src="' + result.image + '" alt="' + result.alt + '"></div><div class="card-label"><div class="card-name">' + result.name + '</div><div class="card-price">$' +
+                result.price + '</div></div></a></div>';
         });
 
         // Set the contents of the table body to the new set of rendered HTML rows
@@ -57,32 +103,9 @@ var carded = 'carded';
             });
         renderList(sortedResults);
     }
-    document.querySelector('#orderBy').addEventListener('change', function (event){
+
+    document.querySelector('#orderBy').addEventListener('change', function (event) {
         orderBy(event.target.value);
-    });
-
-    // i couldn't get this to work. i don't think the code is wrong, it may be because we have these values as strings
-    // (not done after i tried to treat these as strings) this also didn't work but i don't think this is the solution. it might just need to not be strings
-    function orderPrice(sortValue) {
-        var sortedResults = (sortValue === '_price')
-            mockDatabase.sort(function (a, b) {
-                return a[sortValue] - b[sortValue];
-            });
-        renderList(sortedResults);
-    }
-    document.querySelector('#orderPrice').addEventListener('change', function (event){
-        orderPrice(event.target.value);
-    });
-
-    function orderPop(sortValue) {
-        var sortedResults = (sortValue === '_popularity')
-        mockDatabase.sort(function (a, b) {
-            return a[sortValue] - b[sortValue];
-        });
-        renderList(sortedResults);
-    }
-    document.querySelector('#orderPop').addEventListener('change', function (event){
-        orderPop(event.target.value);
     });
 
     // can we make animals and items different drop downs like in the published example
@@ -92,20 +115,22 @@ var carded = 'carded';
         });
         renderList(filteredResults);
     }
-    document.querySelector('#animal').addEventListener('change', function (event){
-        var value = event.target.value === 'true';
-        toggleAnimals(value);
+
+    document.querySelector('#category').addEventListener('change', function (event) {
+        var value = event.target.value === 'item';
+
+    toggleAnimals(value);
     });
 
-    function toggleItems(showItems) {
+    /*function toggleItems(showItems) {
         var filteredResults = mockDatabase.filter(function (result) {
             return showItems || result.item;
         });
         renderList(filteredResults);
     }
-    document.querySelector('#item').addEventListener('change', function (event){
-        var value = event.target.value === 'true';
+    document.querySelector('#category').addEventListener('change', function (event){
+        var value = event.target.value === 'animal';
         toggleItems(value);
-    });
+    }); */
 
 })();
